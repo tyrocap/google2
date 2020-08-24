06bb85e5f4047974e7548ed24c0cf8c63a131b28
adam <asoliyev13@gmail.com>
2020-08-24 19:51:59 -0400

    Added GS/SB

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
+import Search from './components/Search';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Home/>
+          </Route>
+          <Route path="/search">
+            <h1>This is a search page</h1>
+            <Search />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
