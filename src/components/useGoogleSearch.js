573f25929252f3a206d661b4435a8183cd962e3f
adam <asoliyev13@gmail.com>
2020-09-30 13:20:07 -0400

    updated images

--- src/components/useGoogleSearch.js
+++ src/components/useGoogleSearch.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import {useState, useEffect} from 'react';
-import API_KEY from "../keys";
+import React from "react";
+import { useState, useEffect } from "react";
 
+// GOOGLE SEARCH API KEY GOES HERE
+const API_KEY = "your google search api key here";
 const CONTEXT_KEY = "1d74578f6191d4559";
 
 const useGoogleSearch = (term) => {
-  const [data, setData] = useState(null);
+	const [data, setData] = useState(null);
 
-  useEffect(() => {
-    const fetchData = async() => {
-      fetch(
-        `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${term}`
-      )
-        .then(response => response.json())
-        .then(result => {
-          setData(result)
-        })
-    }
+	useEffect(() => {
+		const fetchData = async () => {
+			fetch(
+				`https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${term}`
+			)
+				.then((response) => response.json())
+				.then((result) => {
+					setData(result);
+				});
+		};
 
-    fetchData();
-  }, [term])
-  return {data}
+		fetchData();
+	}, [term]);
+	return { data };
 };
 
 export default useGoogleSearch;
